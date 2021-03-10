1f556b305814e8564daf532f101ef1212ce05cf8
Michael Liu <llc890410@gmail.com>
2021-03-10 23:36:31 +0800

    修改 HelpFragment

--- app/src/main/java/com/example/game03/ui/help/HelpFragment.java
+++ app/src/main/java/com/example/game03/ui/help/HelpFragment.java
@@ -1,38 +1,36 @@
 package com.example.game03.ui.help;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.game03.R;
+import com.example.game03.ui.help.HelpViewModel;
 
 public class HelpFragment extends Fragment {
 
-    private HelpViewModel mViewModel;
+    private HelpViewModel helpViewModel;
 
-    public static HelpFragment newInstance() {
-        return new HelpFragment();
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        helpViewModel =
+                new ViewModelProvider(this).get(HelpViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_help, container, false);
+        final TextView textView = root.findViewById(R.id.text_help);
+        helpViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.help_fragment, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(HelpViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/example/game03/ui/help/HelpViewModel.java
+++ app/src/main/java/com/example/game03/ui/help/HelpViewModel.java
@@ -1,7 +1,19 @@
 package com.example.game03.ui.help;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class HelpViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private MutableLiveData<String> mText;
+
+    public HelpViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is help fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
