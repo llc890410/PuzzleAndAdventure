16aa05ca341db8f87fb06a64ee293d32c73fedd9
Michael Liu <llc890410@gmail.com>
2021-04-08 23:27:00 +0800

    更新 ListData.java

--- app/src/main/java/com/example/game03/model/ListData.java
+++ app/src/main/java/com/example/game03/model/ListData.java
@@ -1,4 +1,25 @@
 package com.example.game03.model;
 
 public class ListData {
+    private int taskId;
+    private String taskName;
+    private boolean isFinished;
+
+    public ListData(int taskId, String taskName, boolean isFinished){
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.isFinished = isFinished;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public String getTaskName(){
+        return taskName;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
 }
